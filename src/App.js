1dc426052864a2c219e6b0119dcd4a349ca1ef73
JulianEaton <50337385+JulianEaton@users.noreply.github.com>
2020-10-19 22:01:24 +0100

    Final commit with cionditional edit added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { v4 } from "uuid";
+class App extends Component {
+  state = {
+    items: [],
+    id: v4(),
+    title: "",
+    editItem: false,
+  };
+  handleChange = (e) => {
+    this.setState({
+      title: e.target.value,
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    if (this.state.title.trim()) {
+      const newItem = {
+        id: this.state.id,
+        title: this.state.title,
+      };
+      const updatedItems = [...this.state.items, newItem];
+
+      this.setState({
+        items: updatedItems,
+        title: "",
+        id: v4(),
+        editItem: false,
+      });
+    }
+  };
+  handleClearList = (e) => {
+    this.setState({ items: [] });
+  };
+  handleDeleteItem = (id) => {
+    const filteredItems = this.state.items.filter((item) => item.id !== id);
+    this.setState({ items: filteredItems });
+  };
+  handleEditItem = (id) => {
+    if (!this.state.editItem) {
+      const itemToEdit = this.state.items.find((item) => item.id === id);
+
+      const filteredItems = this.state.items.filter((item) => item.id !== id);
+      this.setState({
+        items: filteredItems,
+        title: itemToEdit.title,
+        editItem: true,
+        id,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col col-10 mx-auto col-md-8 mt-4">
+            <h3 className="text-capitalize text-center">todo input</h3>
+            <TodoInput
+              title={this.state.title}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.state.editItem}
+            />
+            <TodoList
+              items={this.state.items}
+              handleClearList={this.handleClearList}
+              handleDeleteItem={this.handleDeleteItem}
+              handleEditItem={this.handleEditItem}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import TodoItem from "./TodoItem";
+export default class TodoList extends Component {
+  render() {
+    const { items, handleClearList, handleDeleteItem, handleEditItem } = this.props;
+    return (
+      <ul className="list-group my-5">
+        <h3 className="text-capitalize text-center">todo list</h3>
+        {items.map((item) => {
+          return (
+            <TodoItem
+              key={item.id}
+              title={item.title}
+              handleDeleteItem={() => handleDeleteItem(item.id)}
+              handleEditItem={() => handleEditItem(item.id)}
+            />
+          );
+        })}
+        <button
+          type="button"
+          className="btn btn-danger btn-block text-capitalize mt-5"
+          onClick={handleClearList}
+        >
+          clear list
+        </button>
+      </ul>
+    );
+  }
+}
